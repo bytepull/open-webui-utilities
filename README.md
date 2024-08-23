fc7540a94ba422dda0f5109a6252164ee64f9cf2
lucagiommoni <luca.giommoni91@gmail.com>
2024-08-23 07:36:51 +0200

    First commut

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # open-webui
-scripts to use on open-webui
+Scripts to use on open-webui
+
+[Youtube Transcript API](https://pypi.org/project/youtube-transcript-api/)
+
+`docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main`
---
+++ tools/youtube_video_transcript.py
@@ -0,0 +1,111 @@
+"""
+title: Youtube Video Transcript Retriever
+author: Luca G
+funding_url: https://github.com/open-webui
+version: 1.0.0
+license: MIT
+"""
+
+import re
+from pydantic import BaseModel, Field
+from typing import Callable, Any
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+
+
+class EventEmitter:
+    def __init__(self, event_emitter: Callable[[dict], Any] = None):
+        self.event_emitter = event_emitter
+
+    async def emit(self, description="Unknown State", status="in_progress", done=False):
+        if self.event_emitter:
+            await self.event_emitter(
+                {
+                    "type": "status",
+                    "data": {
+                        "status": status,
+                        "description": description,
+                        "done": done,
+                    },
+                }
+            )
+
+
+class Tools:
+    def __init__(self):
+        pass
+
+    async def get_video_transcript(
+        self, url: str, 
+        __event_emitter__: Callable[[dict], Any] = None) -> str:
+        """
+        Retrieves the transcript for a YouTube video given the video URL.
+        :url: The URL of the YouTube video.
+        :return: The transcript of the YouTube video.
+        """
+
+        emitter = EventEmitter(__event_emitter__)
+        pattern = re.compile(r'https\:\/\/www.youtube.com/watch\?v=[A-Za-z0-9]+')
+        
+        if not(bool(pattern.match(url))):
+            await emitter.emit(
+                status="error",
+                description=f"Wrong URL: {url}",
+                done=True,
+            )
+            return None
+        
+        video_id = url.split("v=")[1]
+        
+        if not(video_id):
+            await emitter.emit(
+                status="error",
+                description=f"Cannot get video ID from URL: {url}",
+                done=True,
+            )
+            return None
+
+        await emitter.emit("Fetching video transcript")
+        formatter = TextFormatter()
+
+        # retrieve the available transcripts
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript_text = ''
+        
+        # iterate over all available transcripts
+        for transcript in transcript_list:
+            if transcript.language_code == 'it':
+                print(transcript.language)
+                transcript_text = formatter.format_transcript(transcript.fetch())
+                break
+        
+        if not(transcript_text):
+            for transcript in transcript_list:
+                if transcript.language_code == 'en':
+                    if transcript.is_translatable:
+                        language = ''
+                        for translation_language in transcript.translation_languages:
+                            if translation_language.language_code == 'it':
+                                language = translation_language.language_code
+                                break
+                        if language:
+                            transcript_text = formatter.format_transcript(transcript.translate(language).fetch())
+                        else:
+                            transcript_text = formatter.format_transcript(transcript.fetch())
+                    else:
+                        transcript_text = formatter.format_transcript(transcript.fetch())
+        
+        if not(transcript_text):
+            await emitter.emit(
+                status="error",
+                description="Transcript not found",
+                done=True,
+            )
+            return None
+        else:
+            await emitter.emit(
+                status="complete",
+                description="Transcript retrieved succesfully",
+                done=True,
+            )
+            return transcript_text
